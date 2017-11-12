52dc05f58e422d6bf684c844aa11f013396b4e72
root <root@tesla.localdomain>
2017-11-12 12:33:03 -0500

    defined purpose of datagenerator function

--- datagenerator.py
+++ datagenerator.py
@@ -1,3 +1,2 @@
-from extract_tags import extract_tags
-
-'''File whose purpose is to extract a dictionary from the tags csv  
+from get_img_dict import get_img_dict
+'''File whose purpose is to preprocess images and produce an even larger dictionary for use as training and test data.'''
