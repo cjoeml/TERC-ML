40fc33a79e9e6b040b76e29088d1f6948b984b60
root <root@tesla.localdomain>
2017-11-12 12:23:21 -0500

    made extract tags work with any directory system, began new trials function, created empty file

---
+++ datagenerator.py
@@ -0,0 +1,3 @@
+from extract_tags import extract_tags
+
+'''File whose purpose is to extract a dictionary from the tags csv  
--- extract_tags.py
+++ extract_tags.py
@@ -3,34 +3,30 @@ from PIL import Image, IptcImagePlugin
 import os
 import pandas as pd
 
-# Constants, configure as appropriate
-EARTH_PHOTOS_DIRECTORY = "./WinEarthPhotosByKeyword/"
-INTERIOR_ISS_DIRECTORY = "./ISS/"
+def extract_tags(directories):
+	for d in directories:
+		filenames_and_tags = ["Filename","Tags"]
+		for subdir, dirs, files in os.walk(d):
+			for f in files:
+					if re.search(".jpg",f):
+						img = Image.open(subdir + "/" + f)
+						keywords = IptcImagePlugin.getiptcinfo(img)[(2, 25)]
 
-directories = [EARTH_PHOTOS_DIRECTORY,INTERIOR_ISS_DIRECTORY]
+						tags = ""
 
-for d in directories:
-	filenames_and_tags = []
-	filenames_and_tags.append(["Filename","Tags"])
+						# if the image has just a single tag
+						if type(keywords) == type(bytes()):
+								tags += str(keywords, encoding="utf-8")
+						else:
+							for k in keywords:
+								tags += str(k, encoding="utf-8")
+								# don't put a comma on the end
+								if k != keywords[len(keywords)-1]:
+									tags += ","
 
-	for subdir, dirs, files in os.walk(d):
-		for f in files:
-				if re.search(".jpg",f):
-					img = Image.open(subdir + "/" + f)
-					keywords = IptcImagePlugin.getiptcinfo(img)[(2, 25)]
-					
-					tags = ""
-					
-					# if the image has just a single tag
-					if type(keywords) == type(bytes()):
-							tags += str(keywords, encoding="utf-8")
-					else:
-						for k in keywords:
-							tags += str(k, encoding="utf-8")
-							# don't put a comma on the end
-							if k != keywords[len(keywords)-1]:
-								tags += ","
+						filenames_and_tags.append([f,tags])
 
-					filenames_and_tags.append([f,tags])
+		pd.DataFrame(filenames_and_tags).to_csv(d + "tags.csv")
 
-	pd.DataFrame(filenames_and_tags).to_csv(d + "tags.csv")
+if __name__ == "__main__":
+    extract_tags(["../data/"])
