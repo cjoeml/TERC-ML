f4c5e72db26f2b2bf2eb2bf668adf2973c93b3bb
root <root@tesla.localdomain>
2017-11-12 22:38:43 -0500

    Edited extract tags, get_img_tag, and datagenerator to be mutally compatible; datagenerator now creates a set of transformed images and returns a dictionary of them and their tags

--- datagenerator.py
+++ datagenerator.py
@@ -1,2 +1,55 @@
 from get_img_dict import get_img_dict
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras import backend as K
+import os
+
 '''File whose purpose is to preprocess images and produce an even larger dictionary for use as training and test data.'''
+
+NUM_TRANSFORMED = 5 '''This is the number of transformed images for each in the original set'''
+
+datagen = ImageDataGenerator(
+  rotation_range = 40,
+  width_shift_range = .2,
+  height_shift_range = .2,
+  rescale = 1/255,
+  shear_range = .2,
+  zoom_range = .2,
+  horizontal_flip = True,
+  fill_mode = 'nearest'
+)
+
+def transform_images(image_dict):
+    '''Produce NUM_TRANSFORMED images that are transformations of the resized data set'''
+    for key, tags  in image_dict.items():
+        img = load_img('../data/raw/{}'.format(key))
+        x = img_to_array(img)
+        x = x.reshape((1, *x.shape))
+
+        #Generate random transformations of image
+        i=0
+        for batch in datagen.flow(x, batch_size=1, save_to_dir='../data/transformed',
+        save_prefix=key.replace(".jpg", ""), save_format='jpg', shuffle=False):
+            i += 1
+            if i > NUM_TRANSFORMED: #gen 50 images for this shiz
+                break  # otherwise the generator would loop indefinitely
+        img_width, img_height = 100,100
+        if K.image_data_format() == 'channels_first':
+            input_shape = (3, img_width, img_height)
+        else:
+            input_shape = (img_width, img_height, 3)
+
+def get_trans_dict(image_dict):
+    '''Get a dictionary representing the tags for a filename key on the transformed images'''
+    trans_dict = {}
+    translist = os.listdir("../data/transformed")
+    for filename, tags in image_dict.items():
+        for transname in translist:
+            if(transname.contains(filename)):
+                trans_dict[transname] = tags
+                translist.remove(transname)
+    return trans_dict
+
+if(__name__ == "__main__"):
+    img_data = get_img_dict("../data")
+    transform_images(img_data)
+    print(get_trans_dict(img_data))
--- get_img_dict.py
+++ get_img_dict.py
@@ -2,16 +2,22 @@ import csv
 
 def get_img_dict(directory):
 
-	with open(directory + 'tags.csv', 'r') as tags:
+	with open(directory + '/tags.csv', 'r') as tags:
 		reader = csv.reader(tags, delimiter=',')
 		img_dict = {}
 		for row in reader:
-			img_dict[row[1]] = row[2].split(',')
-			print(row[1] + ": " + str(img_dict[row[1]]))
+			try:
+				if(row[1] in ["Filename", "Tags"]):
+					continue
+				filename, tags = eval(row[1])
+				tags = tags.split(",")
+				img_dict[filename] = tags
+			except(Exception):
+				pass
 
 		# We should probably double check which tags are relevant or not on Slack
-		removeIrrelevantTags = ['1', 'Tags', 'Windows on Earth', '#WinEarthFavs', 'Astronaut Favs', 'Private', 'BUSpark', 'Christ Hadfield tweet', 
-		'Scott Kelly tweet', 'Anousheh Ansari', 'Anousheh_All_Sun_Moon', 'Anousheh_Earth', 'Anousheh_Iran', 'Reid_Wiseman', 
+		removeIrrelevantTags = ['1', 'Tags', 'Windows on Earth', '#WinEarthFavs', 'Astronaut Favs', 'Private', 'BUSpark', 'Christ Hadfield tweet',
+		'Scott Kelly tweet', 'Anousheh Ansari', 'Anousheh_All_Sun_Moon', 'Anousheh_Earth', 'Anousheh_Iran', 'Reid_Wiseman',
 		'#MovieSession - Reviewed', 'ReidWiseman movie tweet', 'WinEarthTweet', 'Maynard Show', 'BarnesAndNoble', 'ReidFavs', 'Leroy Chiao']
 
 		# Removing unneeded tags that we can't measure
@@ -19,3 +25,6 @@ def get_img_dict(directory):
 		    img_dict.pop(k, None)
 
 		return img_dict
+if __name__ == "__main__":
+	d = get_img_dict("../data")
+	print(d)
