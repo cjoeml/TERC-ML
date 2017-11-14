24d83d766a96895357e4dec98e3545ec82490406
Anthony Inzero <ainzero@bu.edu>
2017-11-13 21:36:06 -0500

    Implemented Baseline CNN learning model

--- learning_model.py
+++ learning_model.py
@@ -1,4 +1,12 @@
 import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD, Adadelta, Adagrad
+from keras.utils import np_utils, generic_utils
+from keras.callbacks import EarlyStopping
+from sklearn.cross_validation import train_test_split
 
 # imports from our repo
 import get_data as io
@@ -36,7 +44,40 @@ def image_preprocessing():
 def main():
 	X_train,X_test,Y_train,Y_test,input_shape = image_preprocessing()
 
+	nb_classes = len(Y_test[0])
 	# TODO(Tony): Bring over code from trial3.py
+	model = Sequential()
+
+	model.add(Convolution2D(32, 3, 3, border_mode='same',input_shape=input_shape))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(32, 3, 3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+
+	model.add(Convolution2D(64, 3, 3, border_mode='same'))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(64, 3, 3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+
+	model.add(Flatten())
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(nb_classes))
+	model.add(Activation('softmax')) # Should probably use this because we're doing multiclass stuff
+
+	# let's train the model using SGD + momentum
+	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) # ty adam
+	early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+	model.fit(X_train,Y_train,nb_epoch=10,batch_size=32,validation_data=(X_test,Y_test),shuffle=True, callbacks=[early_stopping])
+	score = model.evaluate(X_test, Y_test, verbose=0)
+	out = model.predict(X_test)
+
+
 
 if __name__ == "__main__":
     main()
